8d5e13fa872d1ac5a60d4f04ad304dee00e2f735
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2022-09-04 14:35:28 +0300

    Submit by passing any parameter

--- src/submit.sh
+++ src/submit.sh
@@ -2,8 +2,8 @@
 token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pa2hhaWwuZHZvcmtpbkBnbWFpbC5jb20iLCJleHAiOjE2NjIyOTQzOTEsIm9yaWdfaWF0IjoxNjYyMjA3OTkxfQ.XQvEEm_DwOiO8dzU00LCUOLJjTDjtBRBACeCVGQUZqM
 cd ../output/
 rm best.txt
-for i in {01..25} ; do
+for i in {01..35} ; do
 	name=`ls -1 ${i}_?????_* | head -n 1`
 	echo $name >> best.txt
-	#curl --header "Authorization: Bearer $token" -F file=@$name https://robovinci.xyz/api/submissions/$i/create;
+	if [ -n "$1" ]; then echo curl --header "Authorization: Bearer $token" -F file=@$name https://robovinci.xyz/api/submissions/$i/create; fi;
 done
